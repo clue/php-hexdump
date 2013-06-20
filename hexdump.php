ead93411181327d7c7ad484506ff6bb8a4a840be
Christian Lück <christian@lueck.tv>
2013-06-20 23:31:25 +0200

    Add OOP layout

--- hexdump.php
+++ hexdump.php
@@ -1,81 +1,89 @@
 <?php
 
+namespace Clue\Hexdump;
+
 /**
- * View any string as a hexdump.
+ * View any string as a hexdump
  *
  * This is most commonly used to view binary data from streams
  * or sockets while debugging, but can be used to view any string
  * with non-viewable characters.
- *
- * @version     1.3.2
- * @author      Aidan Lister <aidan@php.net>
- * @author      Peter Waller <iridum@php.net>
- * @link        http://aidanlister.com/2004/04/viewing-binary-data-as-a-hexdump-in-php/
- * @param       string  $data        The string to be dumped
- * @param       bool    $htmloutput  Set to false for non-HTML output
- * @param       bool    $uppercase   Set to true for uppercase hex
- * @param       bool    $return      Set to true to return the dump
  */
-function hexdump ($data, $htmloutput = true, $uppercase = false, $return = false)
+class Hexdump
 {
-    // Init
-    $hexi   = '';
-    $ascii  = '';
-    $dump   = ($htmloutput === true) ? '<pre>' : '';
-    $offset = 0;
-    $len    = strlen($data);
-
-    // Upper or lower case hexadecimal
-    $x = ($uppercase === false) ? 'x' : 'X';
-
-    // Iterate string
-    for ($i = $j = 0; $i < $len; $i++)
+    /**
+     * View any string as a hexdump.
+     *
+     * @version     1.3.2
+     * @author      Aidan Lister <aidan@php.net>
+     * @author      Peter Waller <iridum@php.net>
+     * @link        http://aidanlister.com/2004/04/viewing-binary-data-as-a-hexdump-in-php/
+     * @param       string  $data        The string to be dumped
+     * @param       bool    $htmloutput  Set to false for non-HTML output
+     * @param       bool    $uppercase   Set to true for uppercase hex
+     * @param       bool    $return      Set to true to return the dump
+     */
+    function dump ($data, $htmloutput = true, $uppercase = false, $return = false)
     {
-        // Convert to hexidecimal
-        $hexi .= sprintf("%02$x ", ord($data[$i]));
+        // Init
+        $hexi   = '';
+        $ascii  = '';
+        $dump   = ($htmloutput === true) ? '<pre>' : '';
+        $offset = 0;
+        $len    = strlen($data);
 
-        // Replace non-viewable bytes with '.'
-        if (ord($data[$i]) >= 32) {
-            $ascii .= ($htmloutput === true) ?
-            htmlentities($data[$i]) :
-            $data[$i];
-        } else {
-            $ascii .= '.';
-        }
+        // Upper or lower case hexadecimal
+        $x = ($uppercase === false) ? 'x' : 'X';
 
-        // Add extra column spacing
-        if ($j === 7) {
-            $hexi  .= ' ';
-            $ascii .= ' ';
-        }
+        // Iterate string
+        for ($i = $j = 0; $i < $len; $i++)
+        {
+            // Convert to hexidecimal
+            $hexi .= sprintf("%02$x ", ord($data[$i]));
 
-        // Add row
-        if (++$j === 16 || $i === $len - 1) {
-            // Join the hexi / ascii output
-            $dump .= sprintf("%04$x  %-49s  %s", $offset, $hexi, $ascii);
+            // Replace non-viewable bytes with '.'
+            if (ord($data[$i]) >= 32) {
+                $ascii .= ($htmloutput === true) ?
+                htmlentities($data[$i]) :
+                $data[$i];
+            } else {
+                $ascii .= '.';
+            }
 
-            // Reset vars
-            $hexi   = $ascii = '';
-            $offset += 16;
-            $j      = 0;
+            // Add extra column spacing
+            if ($j === 7) {
+                $hexi  .= ' ';
+                $ascii .= ' ';
+            }
 
-            // Add newline
-            if ($i !== $len - 1) {
-                $dump .= "\n";
+            // Add row
+            if (++$j === 16 || $i === $len - 1) {
+                // Join the hexi / ascii output
+                $dump .= sprintf("%04$x  %-49s  %s", $offset, $hexi, $ascii);
+
+                // Reset vars
+                $hexi   = $ascii = '';
+                $offset += 16;
+                $j      = 0;
+
+                // Add newline
+                if ($i !== $len - 1) {
+                    $dump .= "\n";
+                }
             }
         }
-    }
 
-    // Finish dump
-    $dump .= $htmloutput === true ?
-            '</pre>' :
-            '';
-    $dump .= "\n";
+        // Finish dump
+        $dump .= $htmloutput === true ?
+                '</pre>' :
+                '';
+        $dump .= "\n";
 
-    // Output method
-    if ($return === false) {
-        echo $dump;
-    } else {
-        return $dump;
+        // Output method
+        if ($return === false) {
+            echo $dump;
+        } else {
+            return $dump;
+        }
     }
 }
